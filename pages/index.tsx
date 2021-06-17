483d4131cea0244c5b2aa87e1f8be0f555f9fb40
Noel Gomez <noel_gomez@yahoo.com>
2021-06-16 18:29:28 -0700

    Fix Typos

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,7 @@ export default function Index() {
           mt="20"
         >
           <Heading mb={2} textAlign="center" fontSize={["3xl", "5xl", "6xl", "7xl"]} letterSpacing="tighter" fontWeight="extrabold">Deliver reliable insights to production <i>faster</i> </Heading>
-          <Text color={colorSecondary[colorMode]} mt={5} mx={10} fontSize={["sm", "md", "lg", "xl"]} textAlign="center">Setting up an Analytics Engineer's development environment is hard, and having them apply DataOps best practices is even harder. Let's change that 💪</Text>
+          <Text color={colorSecondary[colorMode]} mt={5} mx={10} fontSize={["sm", "md", "lg", "xl"]} textAlign="center">Setting up an Analytics Engineer's development environment is hard and having them apply DataOps best practices is even harder. Let's change that 💪</Text>
 
           <HStack margin="auto" mt={10} mb={10} spacing={5}>
             <Button as="a" href="https://github.com/datacoves" bg={mainButtonColor[colorMode]} color="white" fontWeight="500" px={6} border="1px solid black" _hover={{backgroundColor: "white", color: "black"}}>Start with dbt-coves</Button>
@@ -67,7 +67,7 @@ export default function Index() {
           <Text width="100%" align="center" mb={20}>License: MIT</Text>
 
           <Heading mt={20} mb={2} letterSpacing="tight" size="xl" fontWeight={700} as="h2" textAlign="center" w="100%">Who we are</Heading>
-          <Text width="100%" align="center" mb={10}>We're in a mission to make the Analytics Engineer's life easier and help them implement DataOps.</Text>
+          <Text width="100%" align="center" mb={10}>We're on a mission to make the Analytics Engineer's life easier and help them implement DataOps.</Text>
 
           <Stack spacing={10} my={20} w="100%" direction={{base: 'column', lg: 'row'}} alignItems="center">
             <Flex
